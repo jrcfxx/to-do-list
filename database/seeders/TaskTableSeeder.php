6424440d394dc274ebc6fda46dc50cbdf3f57850
jrcfxx <juliarochafiorini@gmail.com>
2024-09-01 20:29:13 -0300

    Fixing a bug in TaskTableSeeder

--- database/seeders/TaskTableSeeder.php
+++ database/seeders/TaskTableSeeder.php
@@ -26,6 +26,7 @@ class TaskTableSeeder extends Seeder
                 'title' => Str::random(10),
                 'description' => 'testing',
                 'priority' => 5,
+                'status' => 'open',
                 'due_date' => Carbon::createFromFormat('d/m/Y', '15/07/2024')->format('Y-m-d'),
                 'completeness_date' => Carbon::createFromFormat('d/m/Y', '10/07/2024')->format('Y-m-d'),
                 'delete_date' => Carbon::createFromFormat('d/m/Y', '15/07/2024')->format('Y-m-d'),
